758ce12ae05009a93720c8cf9a40f102b798e6ae
Valeriy Dobrov <valeriy.dobrov.98@yandex.ru>
2024-02-07 21:45:59 +0400

    Add plugin

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,7 +1,6 @@
 package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
-
 import org.junit.jupiter.api.Assertions;
 
 public class StatisticsServiceTest {
@@ -12,6 +11,18 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 25, 8, 6, 11, 13, 12};
+        long expected = 25;
 
         long actual = service.findMax(incomesInBillions);
 
